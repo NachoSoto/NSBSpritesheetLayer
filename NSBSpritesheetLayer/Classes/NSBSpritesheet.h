798baf55d791187049f423810490483acf7c61ef
NachoSoto <ignaciosoto90@gmail.com>
2013-08-19 03:03:43 +0000

    Documenting NSBSpritesheet

--- NSBSpritesheetLayer/Classes/NSBSpritesheet.h
+++ NSBSpritesheetLayer/Classes/NSBSpritesheet.h
@@ -13,7 +13,9 @@ typedef const CGRect *NSBSpritesheetFrameSetType;
 @interface NSBSpritesheet : NSObject
 
 /**
- * @param frames and sourceSizes: heap allocated arrays that this class will own.
+ * @param frames: array of CGRects that represent the portion of the `image` for each frame.
+ * @param sourceSizes: array of CGRects that represent how each frame will actually be displayed. The size is tipically the same as that in `frames` and the origin can be offset if the frame has removed white spaces.
+ * @note `frames` and `sourceSizes` need to be heap allocated arrays that this class will own upon calling this method.
  */
 - (id)initWithFrames:(const NSBSpritesheetFrameSetType)frames sourceSizes:(const NSBSpritesheetFrameSetType)sourceSizes frameCount:(const NSUInteger)frameCount spritesheetImage:(UIImage *)image;
 
