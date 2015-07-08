a886a4af3869adf9ad81c8fcfc71c3bf3197c874
Andy Matuschak <andy@khanacademy.org>
2015-07-08 12:45:31 -0700

    Suppress new NSBSpritesheetLayer warning

--- NSBSpritesheetLayer/Classes/NSBSpritesheetLayer.m
+++ NSBSpritesheetLayer/Classes/NSBSpritesheetLayer.m
@@ -35,6 +35,8 @@
 
 @implementation NSBSpritesheetLayer
 
+@dynamic autoreverses; // Implemented by CALayer.
+
 - (instancetype)initWithSpritesheet:(NSBSpritesheet *)spritesheet framesPerSecond:(NSUInteger)framesPerSecond
 {
     NSParameterAssert(spritesheet);
